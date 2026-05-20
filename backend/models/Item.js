e80fe66a8b2aaaad8643c1de33c6732249b8e7da
24f2000042-a11y <24f2000042@ds.study.iitm.ac.in>
2026-05-20 22:16:51 +0530

    update backend/models/Item.js

--- backend/models/Item.js
+++ backend/models/Item.js
@@ -1,6 +1,1 @@
-const mongoose = require('mongoose');
-const itemSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  completed: { type: Boolean, default: false }
-});
-module.exports = mongoose.model('Item', itemSchema);
+const mongoose = require('mongoose'); const itemSchema = new mongoose.Schema({ title: { type: String, required: true }, completed: { type: Boolean, default: false } }); module.exports = mongoose.model('Item', itemSchema);
